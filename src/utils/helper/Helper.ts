071bdfcd093b7bc37236ab0b69364ed7f02c6832
João Pedro <jpmns9103@gmail.com>
2022-02-01 14:04:22 -0300

    instalação e configuração do nodemailer, criação da classe helper e dos corpos de email

---
+++ src/utils/helper/Helper.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+
+class Helper {
+
+	static getEmailEnvironmentVariable(){
+		return process.env.EMAIL;
+	}
+
+	static getEmailPasswordEnvironmentVariable(){
+		return process.env.EMAIL_PASSWORD;
+	}
+
+}
+
+export default Helper;
